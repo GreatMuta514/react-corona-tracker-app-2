b13878d93150fbf68e42848185f97aa808f0667e
Daisuke Shibata <shiba12345678@icloud.com>
2023-09-20 11:43:03 +0900

    APIから特定の国の感染データの取得

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,15 +1,24 @@
 import countriesJson from "../countries.json";
+import { useState } from "react";
+
 
 const Selector = () => {
+    const [country, setCountry] = useState("");
+    const getCountryData = () => {
+        fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
         <div>
-            <select>
+            <select onChange={ (e) => setCountry(e.target.value) }>
+                <option>Select A Country</option>
                 { countriesJson.map( (country, index) =>
-                    <option key={ index }>{ country.Country }</option>
+                    <option key={ index } value={country.Slug}>{ country.Country }</option>
                 ) }
-                {/* <option>United States</option>
-                <option>Japan</option> */}
             </select>
+            <button onClick={ getCountryData }>Get Data</button>
         </div>
     );
 }
