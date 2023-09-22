e39edac45b46ac7f5ca552bfdfd42ccb3510725c
Daisuke Shibata <shiba12345678@icloud.com>
2023-09-22 22:26:16 +0900

    コード整形

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import './App.css';
-import TopPage from './pages/TopPage';
+import "./App.css";
+import TopPage from "./pages/TopPage";
 import { useState } from "react";
 import countriesJson from "./countries.json";
 
-
 function App() {
-
   const [country, setCountry] = useState("");
   const getCountryData = () => {
-      fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }
+    fetch(
+      `https://monotein-books.vercel.app/api/corona-tracker/country/${country}`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div className="App">
-      <TopPage countriesJson={countriesJson}
-               getCountryData={getCountryData}
-               setCountry={setCountry}
-       />
+      <TopPage
+        countriesJson={countriesJson}
+        getCountryData={getCountryData}
+        setCountry={setCountry}
+      />
     </div>
   );
 }
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -3,15 +3,17 @@ import Selector from "../components/Selector";
 import Results from "../components/Results";
 
 const TopPage = (props) => {
-    return (
-        <div>
-            <Title />
-            <Selector countriesJson={props.countriesJson}
-                      getCountryData={props.getCountryData}
-                      setCountry={props.setCountry} />
-            <Results />
-        </div>
-    );
-}
+  return (
+    <div>
+      <Title />
+      <Selector
+        countriesJson={props.countriesJson}
+        getCountryData={props.getCountryData}
+        setCountry={props.setCountry}
+      />
+      <Results />
+    </div>
+  );
+};
 
 export default TopPage;
