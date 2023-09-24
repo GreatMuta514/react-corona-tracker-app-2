15347d0f8619289eca8af9d2c33e9ccf0b5f9708
Daisuke Shibata <shiba12345678@icloud.com>
2023-09-24 16:35:46 +0900

    全世界データをステートに格納まで完了 p100

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Card = () => {
-  return (
-    <div>
-      <h1>Card</h1>
-    </div>
-  );
+    const [allCountriesData, setAllCountriesData] = useState([]);
+    const getAllCountriesData = () => {
+        fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
+        .then(res => res.json())
+        .then(data => setAllCountriesData(data.Countries))
+    }
+    return (
+        <div>
+            <h1>Card</h1>
+            <button onClick={getAllCountriesData}>get all data</button>
+        </div>
+    );
 };
 
 export default Card;
