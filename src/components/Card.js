f32209ff904bfd6770fdaa16c82ceb82b5577f23
Daisuke Shibata <shiba12345678@icloud.com>
2023-09-24 23:36:49 +0900

    全世界・感染データ・表示

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,6 +11,14 @@ const Card = () => {
         <div>
             <h1>Card</h1>
             <button onClick={getAllCountriesData}>get all data</button>
+            {/* {console.log(allCountriesData)} */}
+            {allCountriesData.map((singleData, index) => 
+                <div key={index}>
+                    <h2>{singleData.Country}</h2>
+                    <p>新規感染者：{singleData.NewConfirmed}</p>
+                    <p>感染者総数：{singleData.TotalConfirmed}</p>
+                </div>
+            )}
         </div>
     );
 };
