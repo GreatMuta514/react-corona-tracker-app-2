762940efc721bfd416e2c172bf85ba4ca7c07e91
Daisuke Shibata <shiba12345678@icloud.com>
2023-09-24 23:59:11 +0900

    Titleのレンダリング

--- src/pages/WorldPage.js
+++ src/pages/WorldPage.js
@@ -1,8 +1,10 @@
+import Title from "../components/Title";
 import Card from "../components/Card";
 
 const WorldPage = (props) => {
   return (
     <div>
+      <Title />
       <Card
         allCountriesData={props.allCountriesData}
       />
